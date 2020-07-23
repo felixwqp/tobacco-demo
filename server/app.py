0e57dafd64e0675ad8e8039a887ef66b42773c35
Neo <dnaihao@umich.edu>
2020-07-23 01:22:27 -0400

    Added file path

--- server/app.py
+++ server/app.py
@@ -1,8 +1,35 @@
-from flask import Flask
+from flask import Flask, send_file
 import flask
 from contour_detection import start
 import cv2 as cv
+from PIL import Image
 from dense import start_dense
+import numpy as np
+import io
+import os
+import glob
+
+import flickrapi
+import csv
+import requests
+
+# api_key = '68d7cbd37b544fb8cb4c5049c87064f3'
+# api_secret = '9322150cc690156f'
+# flickr = flickrapi.FlickrAPI(api_key, api_secret)
+# flickr.authenticate_via_browser(perms='delete')
+
+# def uploadPhoto(path_photo, title, description, tags):
+#     result=False
+#     with open(path_photo, 'rb') as f:
+#         photo = {'file': f}
+#         try:    
+#             result = requests.post('https://up.flickr.com/services/upload/', data = {'photo':'photo', 'title':'title', 'description':'description', 'tags':'tags'})
+#             print(result.text)
+#         except Exception as error:
+#             print('Upload photo', error, '--- o --- titulo malo -> ', title)        
+#     return result
+
+
 app = Flask(__name__)
 
 
@@ -25,12 +52,16 @@ def hello(filename):
     image = cv.imread(filename)
     print(filename)
     print("!!!")
-    start_dense()
+    # start_dense()
     # res = {"a":"b"}
-    response = flask.jsonify(res)
-    response.headers.add('Access-Control-Allow-Origin', '*')
-
+    imgs = glob.glob(os.path.join("Width", "*.jpg"))
+    results = list()
+    for idx, img in enumerate(imgs):
+        results.append({"path": os.path.abspath(img), "id": idx})
+    response = flask.jsonify(results)
+    response.headers.add("Access-Control-Allow-Origin", "*")
     return response
+
 
 if __name__ == '__main__':
     app.run()
